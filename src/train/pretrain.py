7dfc9dd0c9e6895a0a572e698a4e3276afb5cdd3
tiizy <tii_zy@yahoo.de>
2021-06-04 19:07:07 +0200

    feat: add first steps of AdamOptimizer to pretrain

--- src/train/pretrain.py
+++ src/train/pretrain.py
@@ -7,29 +7,32 @@ from src.preprocess.dereko.process_raw import PROCESSED_DATA_PATH
 from src.preprocess.utils.json_handler import open_json_file
 import os
 from tqdm import tqdm
+from torch import optim
 
 
+#encoder = BertGenerationEncoder.from_pretrained("bert-base-german-cased", bos_token_id=101, eos_token_id=102)
 encoder = BertGenerationEncoder.from_pretrained("bert-base-german-cased")
-decoder = BertGenerationDecoder.from_pretrained("bert-base-german-cased")
+#decoder = BertGenerationDecoder.from_pretrained("bert-base-german-cased", add_cross_attention=True, is_decoder=True, bos_token_id=101, eos_token_id=102)
+decoder = BertGenerationDecoder.from_pretrained("bert-base-german-cased", is_decoder=True, add_cross_attention=True)
+bert2bert = EncoderDecoderModel(encoder=encoder, decoder=decoder)
 tokenizer = BertTokenizer.from_pretrained("bert-base-german-cased")
 
 def trainBert():
-    #encoder = BertGenerationEncoder.from_pretrained("bert-base-german-cased", bos_token_id=101, eos_token_id=102)
-    #decoder = BertGenerationDecoder.from_pretrained("bert-base-german-cased", add_cross_attention=True, is_decoder=True, bos_token_id=101, eos_token_id=102)
-    bert2bert = EncoderDecoderModel(encoder=encoder, decoder=decoder)
-    
-    #input_ids = tokenizer('This is a long article to summarize', add_special_tokens=False, return_tensors="pt").input_ids
-    #labels = tokenizer('This is a short summary', return_tensors="pt").input_ids
     pairs_path = os.path.join(PROCESSED_DATA_PATH, "pairs.json")
     file_content = open_json_file(pairs_path)
+    optimizer = optim.Adam(params=bert2bert.parameters(), lr=0.001) #params = iterable of parameters to optimize, default betas work well
     pbar = tqdm(total = 100)
     for idx in range(100):
        sentence1 = file_content[idx][0]
        sentence2 = file_content[idx][1]
        input_ids = tokenizer(sentence1, return_tensors="pt").input_ids
+       #input_ids = tokenizer('This is a long article to summarize', add_special_tokens=False, return_tensors="pt").input_ids
        labels = tokenizer(sentence2, return_tensors="pt").input_ids
+       #labels = tokenizer('This is a short summary', return_tensors="pt").input_ids
+       optimizer.zero_grad()
        loss = bert2bert(input_ids=input_ids, decoder_input_ids=labels, labels=labels).loss
        loss.backward()
+       optimizer.step()
        pbar.update(1)
     pbar.close()
 
