530bb187913ef2810ad304f9faba21b9ed177cec
tiizy <tii_zy@yahoo.de>
2021-08-13 14:29:22 +0200

    feat: add a saving function to split training/validation data, fix: remove unneccesary comments

--- src/preprocess/dereko/split_training_validation.py
+++ src/preprocess/dereko/split_training_validation.py
@@ -1,5 +1,9 @@
-from src.preprocess.dereko.bert_tokenization import tokenize_for_bert
+from torch import tensor
 from torch.utils.data import TensorDataset, random_split
+import os
+from src.preprocess.dereko.process_raw import PROCESSED_DATA_PATH
+import torch
+
 
 
 def split_train_validate(input_ids, attention_masks, punctuation_ids) -> TensorDataset:
@@ -13,19 +17,28 @@ def split_train_validate(input_ids, attention_masks, punctuation_ids) -> TensorD
         val_dataset: Dataset for validation
     """
 
-    # Combine the training inputs into a TensorDataset.
+    # Combine the training inputs into a TensorDataset
     dataset = TensorDataset(input_ids, attention_masks, punctuation_ids)
 
-    # Create a 90-10 train-validation split.
+    # Create a 80-20 train-validation split.
 
-    # Calculate the number of samples to include in each set.
-    train_size = int(0.9 * len(dataset))
+    # Calculate the number of samples to include in each set
+    train_size = int(0.8 * len(dataset))
     val_size = len(dataset) - train_size
 
-    # Divide the dataset by randomly selecting samples.
+    # Divide the dataset by randomly selecting samples
     train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
 
     print('{:>5,} training samples'.format(train_size))
     print('{:>5,} validation samples'.format(val_size))
 
     return train_dataset, val_dataset
+
+
+load_path = os.path.join(PROCESSED_DATA_PATH, "tensors")
+input_ids = torch.load(os.path.join(load_path, "input_ids.pt"))
+attention_masks = torch.load(os.path.join(load_path, "attention_masks.pt"))
+punctuation_ids = torch.load(os.path.join(load_path, "punctuation_ids.pt"))
+train_dataset, val_dataset = split_train_validate(input_ids, attention_masks, punctuation_ids)
+torch.save(train_dataset, os.path.join(load_path, "datasets", "training_data.pt"))
+torch.save(val_dataset, os.path.join(load_path, "datasets", "validation_data.pt"))
