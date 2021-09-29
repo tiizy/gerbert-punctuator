fd72f0f5580851f5a0c31f5d4a006f211a885a4b
tiizy <tii_zy@yahoo.de>
2021-09-29 19:35:10 +0200

    fix: revert temporary json merging functionality

--- src/preprocess/dereko/pairs_classification_from_file.py
+++ src/preprocess/dereko/pairs_classification_from_file.py
@@ -5,25 +5,6 @@ from src.preprocess.dereko.generate_pairs_bert_classification import create_clas
 from src.preprocess.dereko.process_raw import PROCESSED_DATA_PATH
 
 def main():
-    file1 = open_json_file(os.path.join(PROCESSED_DATA_PATH, "classification_pairs_less_punct.json"))
-    print(len(file1))
-
-    file2 = open_json_file(os.path.join(PROCESSED_DATA_PATH, "additional_training_pairs_all.json"))
-    print(len(file2))
-    print(file2[165])
-    print(file2[216])
-    print(file2[166])
-    print(file2[217])
-
-    half = int(len(file2) / 2)
-
-    for pair in file2[:half]:
-        file1.append(pair)
-    print(len(file1))
-
-    save_to_json(file1, os.path.join(PROCESSED_DATA_PATH, "classification_pairs_combined_at.json"))
-
-""" def main():
     f = open(os.path.join(PROCESSED_DATA_PATH, "additional_training", "all_shuffled_6.txt"), "r", encoding="utf8")
     file_content = f.readlines()
     f.close()
@@ -32,6 +13,6 @@ def main():
     result_list = []
     for i in range(len(list_y)):
         result_list.append({'X': list_x[i], 'y':list_y[i]})
-    save_to_json(result_list, os.path.join(PROCESSED_DATA_PATH, "additional_training_pairs_6.json")) """
+    save_to_json(result_list, os.path.join(PROCESSED_DATA_PATH, "additional_training_pairs_6.json"))
 
 main()
