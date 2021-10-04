6e4b4984f231539186a46862c6d5ba7ef475c5f3
tiizy <tii_zy@yahoo.de>
2021-10-04 15:59:28 +0200

    feat: add a new method to check the number of punctuation occurrences in JSON files

---
+++ src/preprocess/dereko/check_punctuation_numbers.py
@@ -0,0 +1,22 @@
+import collections
+import os
+from src.preprocess.dereko.process_raw import PROCESSED_DATA_PATH
+from src.preprocess.utils.json_handler import open_json_file
+from collections import Counter
+from src.punctuation_token_id import PUNCTUATION_TOKEN_ID
+
+filename = "additional_training_pairs_1.json"
+path = os.path.join(PROCESSED_DATA_PATH, filename)
+file = open_json_file(path)
+
+punctuation_list = []
+for pair in file:
+    punctuation_list.append(pair['y'])
+
+number_punct = Counter(punctuation_list)
+
+result = {}
+for i in range(8):
+    #result[PUNCTUATION_TOKEN_ID[i]] = int(number_punct[i] * 0.8)
+    result[PUNCTUATION_TOKEN_ID[i]] = int(number_punct[i])
+print(result)
