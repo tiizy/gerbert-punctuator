e846ed2ac391c87e52d1ef64390b51f6eb9cc55a
tiizy <tii_zy@yahoo.de>
2021-10-09 00:35:16 +0200

    feat: add a new method to remove a number of pairs by token_id

--- src/preprocess/dereko/check_punctuation_numbers.py
+++ src/preprocess/dereko/check_punctuation_numbers.py
@@ -5,7 +5,7 @@ from src.preprocess.utils.json_handler import open_json_file
 from collections import Counter
 from src.punctuation_token_id import PUNCTUATION_TOKEN_ID
 
-filename = "temp_filtered.json"
+filename = "classification_pairs_filtered.json"
 path = os.path.join(PROCESSED_DATA_PATH, filename)
 file = open_json_file(path)
 
