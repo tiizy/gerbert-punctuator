cdca4cfe5ceafc648994a169f0f0a078c6264913
tiizy <tii_zy@yahoo.de>
2021-10-09 00:51:51 +0200

    feat: add tqdm to remove_sentences since it can take a while depending on a number

--- src/preprocess/dereko/remove_sentences_by_number.py
+++ src/preprocess/dereko/remove_sentences_by_number.py
@@ -1,24 +1,27 @@
 import os
 from src.preprocess.dereko.process_raw import PROCESSED_DATA_PATH
 from src.preprocess.utils.json_handler import open_json_file, save_to_json
-from collections import Counter
-from src.punctuation_token_id import PUNCTUATION_TOKEN_ID
+from tqdm import tqdm
 
 filename = "temp_filtered.json"
 path = os.path.join(PROCESSED_DATA_PATH, filename)
 file = open_json_file(path)
 
+print(len(file))
+
 #PUNCTUATION_TOKEN_ID = {0:"None", 1:",", 2:".", 3:"?", 4:'"', 5:"(", 6:")", 7:":", 8:"-"}
-number_to_remove = 3
+number_to_remove = 579163
 token_id = 0
 count = 1
 
 reduced_list = file
-for idx, pair in enumerate(file):
+for idx, pair in tqdm(enumerate(file), "Removing pairs"):
     temp = pair['y']
     if count <= number_to_remove:
         if temp == token_id:
             del reduced_list[idx]
             count += 1
+
+print(len(file))
 
 save_to_json(reduced_list, os.path.join(PROCESSED_DATA_PATH, "classification_pairs_filtered.json"))
