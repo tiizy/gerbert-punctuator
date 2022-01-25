e6c2a1945a9f4a7bb3cdeb222fe7a58f479be37f
tiizy <tii_zy@yahoo.de>
2022-01-25 20:38:21 +0100

    fix: correct punct to mask in test

--- tests/test_generate_pairs_bert_classification.py
+++ tests/test_generate_pairs_bert_classification.py
@@ -5,16 +5,16 @@ from src.preprocess.dereko.generate_pairs_bert_classification import create_clas
 def test_create_classification_pairs():
     test_input = ["Peter, wie lange noch?"]
     expected_result = [
-['<PUNCT>', 'Peter', 'wie', 'lange', 'noch'], 
-['Peter', '<PUNCT>', 'wie', 'lange', 'noch'],
-['Peter', ',', 'wie', '<PUNCT>', 'lange', 'noch'],
-['Peter', ',', 'wie', 'lange', '<PUNCT>', 'noch'],
-['Peter', ',', 'wie', 'lange', 'noch', '<PUNCT>']]
+['[MASK]', 'Peter', 'wie', 'lange', 'noch'], 
+['Peter', '[MASK]', 'wie', 'lange', 'noch'],
+['Peter', ',', 'wie', '[MASK]', 'lange', 'noch'],
+['Peter', ',', 'wie', 'lange', '[MASK]', 'noch'],
+['Peter', ',', 'wie', 'lange', 'noch', '[MASK]']]
     actual_result_x, actual_result_y = create_classification_pairs(test_input)
     assert expected_result == actual_result_x
 
 def test_remove_punct_after_marker():
-    test_input = ['Was', 'er', 'offenbar', 'nicht', 'mitgekriegt', 'hat', ',', '<PUNCT>', 'war', ',', 'daß', 'an', 'diesem', 'Abend', 'alle', 'verloren', 'hatten', '.']
-    expected_result = ['Was', 'er', 'offenbar', 'nicht', 'mitgekriegt', 'hat', ',', '<PUNCT>', 'war', 'daß', 'an', 'diesem', 'Abend', 'alle', 'verloren', 'hatten']
+    test_input = ['Was', 'er', 'offenbar', 'nicht', 'mitgekriegt', 'hat', ',', '[MASK]', 'war', ',', 'daß', 'an', 'diesem', 'Abend', 'alle', 'verloren', 'hatten', '.']
+    expected_result = ['Was', 'er', 'offenbar', 'nicht', 'mitgekriegt', 'hat', ',', '[MASK]', 'war', 'daß', 'an', 'diesem', 'Abend', 'alle', 'verloren', 'hatten']
     actual_result = remove_punct_after_marker(test_input)
     assert expected_result == actual_result
