bf4352a57e06e681d9a0ed66d0394ece065fcdcb
tiizy <tii_zy@yahoo.de>
2022-02-01 22:52:25 +0100

    feat: create a method to validate flair, fix: further impove flair inference

--- src/inference/apply_flair.py
+++ src/inference/apply_flair.py
@@ -1,42 +1,34 @@
 from flair.models import TextClassifier
-from flair.datasets import ClassificationCorpus
 import re
 from flair.data import Sentence
-from flair.datasets import DataLoader
 
 model_path = "saved_models/flair-01-02/best-model.pt"
 model = TextClassifier.load(model_path)
 
-data_path = "data/processed/flair/"
-corpus = ClassificationCorpus(data_path,
-                            test_file='flair_test.txt',
-                            dev_file='flair_val.txt',
-                            train_file='flair_train.txt',
-                            label_type="class"                                       
-                            )
-#test_dataloader = DataLoader(corpus.test, batch_size=32)
 
-result, main_score = model.evaluate(corpus.test, gold_label_type=model.label_type)
-print(result.detailed_results)
 
-def flair_inference(sent_list):
+def flair_inference(sentence):
+
     processed_sents = []
-    for sentence in sent_list:
-        list_words = re.findall(r"\w+|[^\w\s]", sentence, re.UNICODE) #look for whole words
-        i = 0
-        while i < len(list_words):
-            masked_list = [el for num, el in enumerate(list_words)] #create list of words
-            masked_list.insert(i, r'[MASK]') #insert the mask between the words   
+    list_words = re.findall(r"\w+|[^\w\s]", sentence, re.UNICODE) #look for whole words
+    for idx, el in enumerate(list_words):
+        masked_list = [el for num, el in enumerate(list_words)] #create list of words
+        masked_list.insert(idx, r'[MASK]') #insert the mask between the words
         processed_sents.append(masked_list)
+    list_words.append(r'[MASK]') #insert the token at the end
+    processed_sents.append(list_words)
+    
     final_list = []
     for item in processed_sents:
-        final_list.append(' '.join(item))
-    print(final_list)
+        final_list.append(' '.join(item)) #convert list to string
+    
+    label_list = []
+    for pos, sent in enumerate(final_list):
+        sentence = Sentence(sent) #create a flair sentence object
+        model.predict(sentence)
+        label = str(sentence.labels)[1]
+        if int(label) != 0:
+            print(pos)
 
-
-#sentence = Sentence("Wann wirst du da sein [MASK]")
-
-#model.predict(sentence)
-#print(sentence.labels)
-
-sent_list = ["Wann wirst du da sein [MASK]", "Dies ist ein Test"]
+sentence = "Dies ist ein Test"
+#flair_inference(sentence)
---
+++ src/inference/validate_flair.py
@@ -0,0 +1,22 @@
+from flair.datasets import ClassificationCorpus
+from flair.models import TextClassifier
+
+model_path = "saved_models/flair-01-02/best-model.pt"
+model = TextClassifier.load(model_path)
+
+def flair_validation():
+
+    data_path = "data/processed/dereko/"
+    corpus = ClassificationCorpus(data_path,
+                                test_file='flair_test_test.txt',
+                                dev_file='flair_val.txt',
+                                train_file='flair_train.txt',
+                                label_type="class"                                       
+                                )
+
+    result = model.evaluate(corpus.test, gold_label_type=model.label_type)
+    
+    with open("saved_models/flair_manual_log.txt", "w") as f:
+        f.write(result.detailed_results)
+    
+flair_validation()
